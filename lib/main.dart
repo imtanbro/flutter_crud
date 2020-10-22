3e1c7322c232a2962198b7f04cce7f020c95ed8e
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-22 22:05:30 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/Login/login.dart';
-import 'package:flutter_crud/Signup/signup.dart';
+
 
 import 'package:flutter_crud/src/app.dart';
 
