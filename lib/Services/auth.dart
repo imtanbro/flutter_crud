727af019d4521447a11717291cb0f9e496da2b9d
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-24 23:04:24 +0530

    Adding Input

--- lib/Services/auth.dart
+++ lib/Services/auth.dart
@@ -6,19 +6,47 @@ class Authentication extends StatefulWidget {
 }
 
 class _AuthenticationState extends State<Authentication> {
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          color: Colors.blueAccent
+        decoration: BoxDecoration(color: Colors.blueAccent),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 200),
+          child: Column(children: [
+          TextFormField(
+            controller: _email,
+            decoration: InputDecoration(
+              hintText: "Something@email.com",
+              labelText: "Email",
+              labelStyle: TextStyle(
+                color: Colors.white,
+              ),
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          TextFormField(
+            controller: _password,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "Password",
+              labelStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            
+          )
+          ],),
         ),
-        child: Center(child: Text("Hello World")),
       ),
-      
     );
   }
 }
