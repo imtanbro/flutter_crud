11b8e1734fbd0d04f9bf9700c655f8438229f8e2
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-24 23:39:17 +0530

    Pre defined Error message of Firebase
    !IMPORTANT

--- lib/Services/firestore_services.dart
+++ lib/Services/firestore_services.dart
@@ -1,24 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_crud/src/Model/entry.dart';
 
 class FireStoreService {
   FirebaseFirestore _db = FirebaseFirestore.instance;
 
   //Get Entries
-  Stream<List<Entry>> getEntries(){
-    return _db
-    .collection('entries')
-    .snapshots()
-    .map((snapshot) => snapshot.docs.map((doc) => Entry.fromJson(doc.data()))
-    .toList());
+  Stream<List<Entry>> getEntries() {
+    return _db.collection('entries').snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => Entry.fromJson(doc.data())).toList());
   }
 
-  //Insert 
-
-
+  //Insert
 
   //Update
 
-
   //Delete
+}
+
+Future<void> signIn(String email, String password) async {}
+Future<void> register(String email, String password) async {
+  try {} on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print("The password is too weak");
+    }
+  } catch (e) {}
 }
