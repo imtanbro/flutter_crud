66cb5de8030c44ac1ea730c28d7bcc61ec93b0c7
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-24 20:55:50 +0530

    Added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text("Home Page"),),
-      ),
-      theme: ThemeData(accentColor: Colors.pinkAccent, primaryColor: Colors.black),
+    return Container(
+      
     );
   }
 }
