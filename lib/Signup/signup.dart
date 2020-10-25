d031777681a2868464088940d04e0b6c84333d4b
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-25 22:28:22 +0530

    ,

--- lib/Signup/signup.dart
+++ lib/Signup/signup.dart
@@ -149,6 +149,7 @@ class _RegisterState extends State<Register> {
                       style: TextStyle(color: Colors.white),
                     ),
                     value: branch,
+                    
                   ),
                   SizedBox(
                     width: 40,
