5dc2c6a621ee8eb163e01085dfaa53575c0f0e33
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-28 23:27:08 +0530

    .

--- lib/Services/firestore_services.dart
+++ lib/Services/firestore_services.dart
@@ -29,48 +29,7 @@ Future<bool> signIn(String email, String password) async {
   //   displayToastMessage("Error: " + onError.toString());
   // });
 
-  // try {
-  //   await FirebaseAuth.instance
-  //       .signInWithEmailAndPassword(email: email, password: password);
-  //
-  //   return true;
-  // } catch (e) {
-  //   authProblems errorType;
-  //   if (Platform.isAndroid) {
-  //     switch (e.message) {
-  //       case 'There is no user record corresponding to this identifier. The user may have been deleted.':
-  //         errorType = authProblems.UserNotFound;
-  //         break;
-  //       case 'The password is invalid or the user does not have a password.':
-  //         errorType = authProblems.PasswordNotValid;
-  //         break;
-  //       case 'A network error (such as timeout, interrupted connection or unreachable host) has occurred.':
-  //         errorType = authProblems.NetworkError;
-  //         break;
-  //       // ...
-  //       default:
-  //         print('Case ${e.message} is not yet implemented');
-  //     }
-  //   } else if (Platform.isIOS) {
-  //     switch (e.code) {
-  //       case 'Error 17011':
-  //         errorType = authProblems.UserNotFound;
-  //         break;
-  //       case 'Error 17009':
-  //         errorType = authProblems.PasswordNotValid;
-  //         break;
-  //       case 'Error 17020':
-  //         errorType = authProblems.NetworkError;
-  //         break;
-  //       // ...
-  //       default:
-  //         print('Case ${e.message} is not yet implemented');
-  //     }
-  //   }
-  //
-  //   print('The error is $errorType');
-  //   return false;
-  // }
+  
 }
 
 Future<bool> register(String email, String password) async {
