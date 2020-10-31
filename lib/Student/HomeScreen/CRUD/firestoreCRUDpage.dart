39cd829893cc15521b72e11ab6acd595b66a198b
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-31 17:25:09 +0530

    Text field

--- lib/Student/HomeScreen/CRUD/firestoreCRUDpage.dart
+++ lib/Student/HomeScreen/CRUD/firestoreCRUDpage.dart
@@ -12,10 +12,47 @@ class _FirestoreCRUDPageState extends State<FirestoreCRUDPage> {
   final db = FirebaseFirestore.instance;
   final _formKey = GlobalKey<FormState>();
 
+  TextFormField buildTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Name",
+        hintText: "Name",
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.pink, width: 2.0),
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+      ),
+      style: TextStyle(
+        color: Colors.black,
+        decorationColor: Color.fromRGBO(66, 133, 244, 1),
+      ),
+      validator: (val) {
+        if (val.isEmpty) {
+          return "Please enter some text";
+        }
+      },
+      onSaved: (newValue) => name = newValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: NavbarCustom("CRUD"),
+      body: ListView(
+        padding: EdgeInsets.all(8),
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                
+                buildTextFormField(),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
