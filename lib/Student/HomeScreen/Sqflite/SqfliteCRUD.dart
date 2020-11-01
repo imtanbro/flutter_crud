dda083ba7d7216d867e051c2fde44173de3d5ace
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-11-01 22:18:37 +0530

    CRUD

--- lib/Student/HomeScreen/Sqflite/SqfliteCRUD.dart
+++ lib/Student/HomeScreen/Sqflite/SqfliteCRUD.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class SqlfliteCRUD extends StatefulWidget {
+  @override
+  _SqlfliteCRUDState createState() => _SqlfliteCRUDState();
+}
+
+class _SqlfliteCRUDState extends State<SqlfliteCRUD> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
