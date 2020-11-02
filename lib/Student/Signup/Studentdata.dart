64ec0a2be4272cfd4520a5ccb1c1167817fdf7a1
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-11-02 22:48:09 +0530

    Trying to use Provider

--- lib/Student/Signup/Studentdata.dart
+++ lib/Student/Signup/Studentdata.dart
@@ -1,3 +1,16 @@
-class StudentData(){
-  
+class StudentData {
+  String fname,
+      mname,
+      lname,
+      div,
+      semister,
+      branch,
+      aboutyou = "",
+      rollno,
+      studentId;
+  int attendace = 0;
+  DateTime pickeddate;
+
+
+  StudentData({this.aboutyou,this.attendace,this.branch})
 }
