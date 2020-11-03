044ab67e6e0b067c64c64476a32645c9ae7906d5
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-11-03 12:29:13 +0530

    Using multi provider

---
+++ lib/Teacher/provider.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/Teacher/Register/auth.dart';
+import 'package:provider/provider.dart';
+
+class AuthForTeacher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(create: (_) => AuthService(FirebaseAuth.instance),
+        
+        ),
+
+        StreamProvider(create: (context) => context.read<AuthService>().authStateChanges,)
+      ],
+      child: MaterialApp(
+        title: "Teacher Auth withProvider",
+        home: AuthWrapper(),
+      ),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container();
+  }
+}
