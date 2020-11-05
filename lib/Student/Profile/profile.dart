cb75836fbca73600cab661591a253c1a785f4a7e
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-11-05 23:19:54 +0530

    a

--- lib/Student/Profile/profile.dart
+++ lib/Student/Profile/profile.dart
@@ -4,62 +4,75 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/widgets/widgets.dart';
 
-class Profile extends StatefulWidget {
+// class Profile extends StatefulWidget {
+//   String div, semister, branch, studentId;
+//   Profile({this.branch, this.div, this.semister, this.studentId});
+//   @override
+//   _ProfileState createState() => _ProfileState();
+// }
+
+// class _ProfileState extends State<Profile> {
+//   QuerySnapshot profileSnapshot;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: NavbarCustom("Profile"),
+//         body: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             profileSnapshot == null
+//                 ? Container(
+//                     child: Center(
+//                       child: CircularProgressIndicator(),
+//                     ),
+//                   )
+//                 : StreamBuilder(
+//                     stream: FirebaseFirestore.instance
+//                         .collection("Branch")
+//                         .doc(widget.branch)
+//                         .collection(widget.semister)
+//                         .doc(widget.div)
+//                         .collection("Student Information")
+//                         .doc(widget.studentId)
+//                         .snapshots(),
+//                     builder: (context, snapshot) {
+//                       if (!snapshot.hasData) {
+//                         Container(
+//                           child: Text("No Data"),
+//                         );
+//                       }
+
+//                       ListView.builder(
+//                         padding:
+//                             EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+//                         shrinkWrap: true,
+//                         physics: ClampingScrollPhysics(),
+//                         itemCount: snapshot.data.documents.length,
+//                         itemBuilder: (context, index) {
+//                           DocumentSnapshot course =
+//                               snapshot.data.documents[index];
+//                           return DisplayUserData(
+//                             name: course['FirstName'],
+//                           );
+//                         },
+//                       );
+//                     },
+//                   )
+//           ],
+//         ));
+//   }
+// }
+
+
+
+class Profile extends StatelessWidget {
   String div, semister, branch, studentId;
   Profile({this.branch, this.div, this.semister, this.studentId});
   @override
-  _ProfileState createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
-  QuerySnapshot profileSnapshot;
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: NavbarCustom("Profile"),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            profileSnapshot == null
-                ? Container(
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  )
-                : StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                        .collection("Branch")
-                        .doc(widget.branch)
-                        .collection(widget.semister)
-                        .doc(widget.div)
-                        .collection("Student Information")
-                        .doc(widget.studentId)
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        Container(
-                          child: Text("No Data"),
-                        );
-                      }
-
-                      ListView.builder(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 10),
-                        shrinkWrap: true,
-                        physics: ClampingScrollPhysics(),
-                        itemCount: snapshot.data.documents.length,
-                        itemBuilder: (context, index) {
-                          DocumentSnapshot course =
-                              snapshot.data.documents[index];
-                          return DisplayUserData(
-                            name: course['FirstName'],
-                          );
-                        },
-                      );
-                    },
-                  )
-          ],
-        ));
+    return Container(
+      
+    );
   }
 }
 
